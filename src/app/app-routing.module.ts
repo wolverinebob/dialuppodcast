ed5d25e47838e36f0af5388e895996df369d9522
Wolverinebob <WVB@WVBa.local>
2020-09-25 19:41:07 -0700

    no backslash leads to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
   {path: "home", component: HomeComponent},
   {path: "about", component: AboutComponent},
   {path: "contact", component: ContactPageComponent} 
