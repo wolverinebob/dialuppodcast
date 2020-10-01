7275185ff664a78fed59bd8a375bd5303ce740e6
Wolverinebob <WVB@WVBa.local>
2020-10-01 14:58:30 -0700

    add YouTube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
   {path: "home", component: HomeComponent},
   {path: "about", component: AboutComponent},
-  {path: "contact", component: ContactPageComponent}, 
+  {path: "contact-us", component: ContactPageComponent}, 
   {path: "social-media", component: SocialMediaComponent}
 ];
 
